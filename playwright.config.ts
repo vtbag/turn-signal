5d11b38cdc86be0d9a4b13256d4a7b16e240dd12
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-04-05 14:05:42 +0200

    Ubuntu to new for Safari

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,20 +2,24 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
 	testDir: './tests',
-	workers:	1,
+	workers: 1,
 	webServer: {
 		command: 'npm run start',
 		port: 3000,
 		reuseExistingServer: !process.env.CI,
 	},
-	projects: [
+	projects: process.env.CI ? [
 		{
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'], channel: 'chrome', },
 		},
 		{
 			name: 'webkit',
-			use: { ...devices['Desktop Safari'] , browserName: 'webkit' },
+			use: { ...devices['Desktop Safari'], browserName: 'webkit' },
 		},
-	],
+	] : [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'], channel: 'chrome', },
+		}],
 });
